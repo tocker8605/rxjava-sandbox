0b5514420d9dfe7c08f71c0696c253404ce9f05f
tocker <tocker8605@gmail.com>
2019-05-19 15:18:10 +0900

    commit base class #2

--- src/main/java/my/tocker/rxsandbox/controller/SomeController.java
+++ src/main/java/my/tocker/rxsandbox/controller/SomeController.java
@@ -1,4 +1,24 @@
 package my.tocker.rxsandbox.controller;
 
+import my.tocker.rxsandbox.dto.SomeObject;
+import my.tocker.rxsandbox.service.ReactiveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/test")
 public class SomeController {
+
+    ReactiveService reactiveService;
+
+    @Autowired
+    public SomeController(ReactiveService reactiveService) {
+        this.reactiveService = reactiveService;
+    }
+
+    @GetMapping
+    public SomeObject response() {
+        return reactiveService.fetchSomeObject();
+    }
+
 }
--- src/main/java/my/tocker/rxsandbox/service/ReactiveService.java
+++ src/main/java/my/tocker/rxsandbox/service/ReactiveService.java
@@ -1,7 +1,25 @@
 package my.tocker.rxsandbox.service;
 
+import my.tocker.rxsandbox.dto.SomeObject;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class ReactiveService {
+
+    private static final Map<String, SomeObject> DATAMAP;
+    static {
+        Map<String, SomeObject> initMap = new HashMap<>();
+
+        initMap.put("1", new SomeObject("tocker", 20));
+        initMap.put("2", new SomeObject("sucker", 30));
+
+        DATAMAP = Collections.unmodifiableMap(initMap);
+    }
+
+    public List<SomeObject> fetchSomeObject() {
+        return new ArrayList<>(DATAMAP.values());
+    }
+
 }
